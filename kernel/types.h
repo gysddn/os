c07f190661c2ad38c8297bd0058fdd2cdfa47e56
gysddn <gysddn@gmail.com>
2021-10-22 10:44:37 +0300

    Add types header.
    Add typedefs for unsigned and signed 8 bits, 16 bits and 32 bits
    data types.

---
+++ kernel/types.h
@@ -0,0 +1,14 @@
+/*
+ * Types
+ * @gysddn <gysddn@gmail.com>
+ *
+ */
+
+typedef unsigned char   u8;
+typedef char            s8;
+
+typedef unsigned short  u16;
+typedef short           s16;
+
+typedef unsigned int    u32;
+typedef int             s32;
