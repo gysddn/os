ffd187c7fcc2d6fa3da1386a8c1a1ef2b7cad4e9
gysddn <gysddn@gmail.com>
2021-11-09 15:52:24 +0300

    Add libc to the project.
    Add libc folder and add compiler flags to prevent the use of glibc
    packed in gcc. I'll be building the libc as I go.

---
+++ libc/include/stdbool.h
@@ -0,0 +1,16 @@
+/*
+ * Standard C Library stdbool.h header file
+ * This code is written in accordance with the C Programming Language
+ * Standard defined in ISO/IEC 9899:2017.
+ * @gysddn <gysddn@gmail.com>
+ *
+ */
+
+#pragma once
+
+#define bool _Bool
+
+#define true  1
+#define false 0
+
+#define __bool_true_false_are_defined 1
