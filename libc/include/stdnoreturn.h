6d2c4d5b527b946dabc63f68247705f8526e18e2
Gıyaseddin <gysddn@gmail.com>
2021-12-06 10:32:04 +0300

    libc: Add stdnoreturn.h

---
+++ libc/include/stdnoreturn.h
@@ -0,0 +1,11 @@
+/*
+ * Standard C Library stdnoreturn.h header file
+ * This code is written in accordance with the C Programming Language
+ * Standard defined in ISO/IEC 9899:2017.
+ * @gysddn <gysddn@gmail.com>
+ *
+ */
+
+#pragma once
+
+#define noreturn _Noreturn
