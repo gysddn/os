f60f9d56ad0ae745b6b8c94f16fc27e5fe38022d
Gıyaseddin <gysddn@gmail.com>
2022-07-11 12:09:17 +0300

    update kernel main for cpp

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <stdint.h>
 
-extern "C" void kernel_main(void) {
+namespace kernel {
+
+ extern "C" [[noreturn]] void kernel_main(void) {
   vga_init();
   uint8_t color = attr_make_color(VGA_BIOS_COLOR_LIGHT_BLUE, VGA_BIOS_COLOR_WHITE_HI);
   vga_putchar('H', color, 0, 0);
@@ -12,10 +14,8 @@ extern "C" void kernel_main(void) {
   vga_putchar('l', color, 2, 0);
   vga_putchar('l', color, 3, 0);
   vga_putchar('o', color, 4, 0);
-  const char * message = "Hello\n";
-  print_debug(message);
 
-  char buf[10];
-  int_to_string(buf, -1234567);
-  print_debug(buf);
+  while(true);
 }
+
+} //End namespace kernel
