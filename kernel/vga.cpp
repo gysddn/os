efce3a5653a0966f0db911b72fb90d017d53d227
Gıyaseddin <gysddn@gmail.com>
2022-11-28 22:02:18 +0300

    VGA: rewrite register management in classes
    reimplement the register management system in c++ classes
    they have not been used so no breaking
    all types of registers(in terms of reading/writing) is done

--- kernel/vga.cpp
+++ kernel/vga.cpp
@@ -5,199 +5,14 @@
  */
 
 #include "vga.h"
+#include "io.h"
 
 /*
  * REGISTER MANAGEMENT
  */
 
-typedef struct {
-  uint16_t readPort;
-  uint16_t writePort;
-} BasicRegister;
 
-typedef struct {
-  uint16_t readPortMono;
-  uint16_t readPortColor;
-  uint16_t writePortMono;
-  uint16_t writePortColor;
-} ExtendedRegister;
-
-typedef struct {
-  uint16_t addressPort;
-  uint16_t dataPort;
-  uint8_t index;
-} IndexedRegister;
-
-typedef struct {
-  struct {
-    uint16_t addressPort;
-    uint16_t dataPort;
-  } pair0;
-  struct {
-    uint16_t addressPort;
-    uint16_t dataPort;
-  } pair1;
-  uint8_t index;
-} CRTRegister;
-
-/*
- * AttributeRegister has the same fields as IndexedRegister
- * but AttributeRegister has a more confusing way of using them
- * so they will be processed differently.
- */
-typedef IndexedRegister AttributeRegister;
-
-
-static inline uint8_t _read_vga_port(const uint16_t port) {
-  uint8_t value;
-  __asm__("mov %1, %%dx\n"
-          "in %%dx, %%al\n"
-          "movb %%al, %0\n"
-          : "=r" (value)
-          : "r" (port)
-          : "%eax", "edx"
-          );
-  return value;
-}
-
-/*
- * This function *just* writes to an I/O port, there can be no say as to whether
- * the operation was successful or not. Therefore it should not be used on its own
- * but in a wrapper where this sort of mechanism takes place.
- */
-static void _write_vga_port(const uint16_t port, const uint8_t value) {
-  __asm__("mov %1, %%dx\n"
-          "movb %0, %%al\n"
-          "out %%al, %%dx\n"
-          :
-          :"r" (value),  "r" (port)
-          : "%eax", "edx"
-          );
-}
-
-static uint8_t _write_vga_port_safe(const uint16_t port, const uint8_t value) {
-  _write_vga_port(port, value);
-
-  if (_read_vga_port(port) == value) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-
-inline static uint8_t _read_reg_basic(const BasicRegister reg) {
-  return _read_vga_port(reg.readPort);
-}
-
-inline static uint8_t _write_reg_basic(const BasicRegister reg, const uint8_t value) {
-  return _write_vga_port_safe(reg.readPort, value);
-}
-
-inline static uint8_t _read_reg_ext_mono(const ExtendedRegister reg) {
-  return _read_vga_port(reg.readPortMono);
-}
-
-inline static uint8_t _read_reg_ext_color(const ExtendedRegister reg) {
-  return _read_vga_port(reg.readPortColor);
-}
-
-inline static uint8_t _write_reg_ext_mono(const ExtendedRegister reg, const uint8_t value) {
-  return _write_vga_port_safe(reg.writePortMono, value);
-}
-
-inline static uint8_t _write_reg_ext_color(const ExtendedRegister reg, const uint8_t value) {
-  return _write_vga_port_safe(reg.writePortColor, value);
-}
-
-inline static uint8_t _read_reg_idx(const IndexedRegister reg) {
-  _write_vga_port_safe(reg.addressPort, reg.index);
-  return _read_vga_port(reg.dataPort);
-}
-
-inline static uint8_t _write_reg_idx(const IndexedRegister reg, const uint8_t value) {
-  _write_vga_port_safe(reg.addressPort, reg.index);
-  return _write_vga_port_safe(reg.dataPort, value);
-}
-
-//TODO Disable "CRTC Registers Protect Enable" field because the kernel will not allow
-//any access to VGA other than the interface provided by vga.h
-static uint8_t _read_reg_crt(const CRTRegister reg) {
-  //Read Miscellaneous Output Register and mask with 1 to get
-  //I/O Access field, which dictates which pair to use
-  uint8_t io_addr_sel = _read_vga_port(0x3CC) & 0x1;
-  if (io_addr_sel == 0) {
-    _write_vga_port_safe(reg.pair0.addressPort, reg.index);
-    return _read_vga_port(reg.pair0.dataPort);
-  } else {
-    _write_vga_port_safe(reg.pair1.addressPort, reg.index);
-    return _read_vga_port(reg.pair1.dataPort);
-  }
-}
-
-static uint8_t _write_reg_crt(const CRTRegister reg, const uint8_t value) {
-  //Read Miscellaneous Output Register and mask with 1 to get
-  //I/O Access field, which dictates which pair to use
-  uint8_t io_addr_sel = _read_vga_port(0x3CC) & 0x1;
-  if (io_addr_sel == 0) {
-    _write_vga_port_safe(reg.pair0.addressPort, reg.index);
-    return _write_vga_port_safe(reg.pair0.dataPort, value);
-  } else {
-    _write_vga_port_safe(reg.pair1.addressPort, reg.index);
-    return _write_vga_port_safe(reg.pair1.dataPort, value);
-  }
-}
-
-//AttributeRegister uses the same port for writes to address and data ports, with a flip-flop
-//inside the card holding the state. There is no standard way to access it but it is reset 
-//by reading the external register InputStatus #1. TODO handle interrupts here.
-static uint8_t _read_reg_atr(const AttributeRegister reg) {
-  //read InputStatus #1 to reset flip-flop
-  _read_vga_port(0x3BA);
-
-  _write_vga_port_safe(reg.addressPort, reg.index);
-  return _read_vga_port(reg.dataPort);
-}
-
-static uint8_t _write_reg_atr(const AttributeRegister reg, const uint8_t value) {
-  //read InputStatus #1 to reset flip-flop
-  _read_vga_port(0x3BA);
-
-  _write_vga_port_safe(reg.addressPort, reg.index);
-  //The .dataPort is only available for reading, writing to dataPort is done
-  //via .addressPort
-  return _write_vga_port_safe(reg.addressPort, value);
-}
-/*
- * The external registers have different ports to read from and write to, for that reason
- * this separate safe function is added.
- */
-static uint8_t _write_ext_vga_port(const uint16_t write_port, const uint16_t read_port, const uint8_t value) {
-  _write_vga_port(write_port, value);
-
-  if (_read_vga_port(read_port) == value) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-
-static inline uint8_t _read_ext_vga_feature(const uint16_t port, const uint8_t offset, const uint8_t length) {
-  /*
-   * @offset is 0-7 inclusive: start position of first bit
-   */
-  return _read_vga_port(port) >> offset & ((1 << length)-1);
-}
-
-static inline uint8_t _write_ext_vga_feature(const uint16_t write_port, const uint16_t read_port, const uint8_t offset, const uint8_t length, uint8_t value) {
-  /*
-   * @offset is 0-7 inclusive: start position of first bit
-   * @value should be alligned to the lowest order bit. Bits extending length will be ignored.
-   */
-  uint8_t mask = (1 << length)-1;
-  value = (value & mask) << offset;
-  uint8_t reg_value = (_read_vga_port(read_port) & ( ~(mask << offset) )) | value;
-  return _write_ext_vga_port(write_port, read_port, reg_value);
-}
+namespace kernel { //Namespace kernel begin
 
 uint16_t* alp_num_mem = (uint16_t*)0xB8000;
 
@@ -220,3 +35,107 @@ void vga_putchar(uint8_t character, uint8_t attr, uint8_t position_w, uint8_t po
 uint8_t attr_make_color(uint8_t bg, uint8_t fg) {
   return bg << 4 | fg;
 }
+
+
+optional<uint8_t> VGARegister::state() {
+  return _state;
+}
+
+optional<uint8_t> BasicRegister::read() {
+  auto value = in8(_read_port);
+  //TODO check for failure
+  _state = value;
+  return value;
+}
+
+void BasicRegister::write(uint8_t value) {
+  out8(_write_port, value);
+  //TODO check for failure
+  //read back again to see if they are equal
+}
+
+optional<uint8_t> ExtendedRegister::read() {
+  uint8_t value{};
+  if (mode() == ExtendedRegister::Mode::Color) {
+    value = in8(_read_port_color);
+  } else {
+    value = in8(_read_port_mono);
+  }
+
+  _state = value;
+  return value;
+}
+
+void ExtendedRegister::write(uint8_t value) {
+  if (mode() == ExtendedRegister::Mode::Color) {
+    out8(_write_port_color, value);
+  } else {
+    out8(_write_port_mono, value);
+  }
+}
+
+optional<uint8_t> IndexedRegister::read() {
+  out8(_address_port, _index);
+  auto value = in8(_data_port);
+  _state = value;
+  return value;
+}
+
+void IndexedRegister::write(uint8_t value) {
+  out8(_address_port, _index);
+  out8(_data_port, value);
+}
+
+optional<uint8_t> CRTRegister::read() {
+ //Read Miscellaneous Output Register and mask with 1 to get
+ //I/O Access field, which dictates which pair to use
+ auto io_addr_sel = in8(MISC_OUT_REG) & 0x1;
+ uint8_t value{};
+ if (io_addr_sel == 0) {
+   out8(_pair0.addressPort, _index);
+   value = in8(_pair0.dataPort);
+ } else {
+   out8(_pair1.addressPort, _index);
+   value =  in8(_pair1.dataPort);
+ }
+  _state = value;
+  return value;
+}
+
+void CRTRegister::write(uint8_t value) {
+ //Read Miscellaneous Output Register and mask with 1 to get
+ //I/O Access field, which dictates which pair to use
+ auto io_addr_sel = in8(MISC_OUT_REG) & 0x1;
+ if (io_addr_sel == 0) {
+   out8(_pair0.addressPort, _index);
+   out8(_pair0.dataPort, value);
+ } else {
+   out8(_pair1.addressPort, _index);
+   out8(_pair1.dataPort, value);
+ }
+}
+
+optional<uint8_t> AttributeRegister::read() {
+  uint8_t value{};
+  //read InputStatus #1 to reset flip-flop
+  in8(ATTR_FLIP_FLOP);
+
+  out8(_address_port, _index);
+  value = in8(_data_port);
+
+  _state = value;
+  return value;
+}
+
+void AttributeRegister::write(uint8_t value) {
+
+  //read InputStatus #1 to reset flip-flop
+  in8(ATTR_FLIP_FLOP);
+
+  //The data port is only available for reading, writing to dataPort is done
+  //via address port
+  out8(_address_port, _index);
+  out8(_address_port, value);
+}
+
+}; //Namespace kernel end
--- kernel/vga.h
+++ kernel/vga.h
@@ -5,7 +5,15 @@
  */
 #pragma once
 
+#include "util/optional.h"
+
 #include <stdint.h>
+
+#define MISC_OUT_REG   0x3CC
+#define ATTR_FLIP_FLOP 0x3BA
+
+
+namespace kernel { //Namespace kernel begin
 
 enum {
   VGA_BIOS_COLOR_BLACK         = 0x0,
@@ -38,3 +46,136 @@ void vga_flush();
 void vga_putchar(uint8_t character, uint8_t attr, uint8_t position_w, uint8_t position_h);
 
 uint8_t attr_make_color(uint8_t bg, uint8_t fg);
+
+class VGA {
+  uint16_t* alp_num_mem = (uint16_t*)0xB8000;
+};
+
+
+class VGARegister {
+public:
+
+  optional<uint8_t> state();
+
+  /*
+   * These functions do not exist and for documentation only.
+   * They are meant to be virtual but since there is not memory management yet,
+   * it is not possible.
+   */
+
+  /* optional<uint8_t> read(); */
+
+  //TODO error state, a Result type implementation??
+  /* void write(uint8_t); */
+
+  /* optional<uint8_t> state(); */
+
+protected:
+  //Can't use virtual functions, so disable the instantiation of this class
+  //out of child classes
+  VGARegister() = default;
+
+  //read() will update this value as well
+  optional<uint8_t> _state;
+};
+
+class BasicRegister : public VGARegister {
+public:
+  BasicRegister() = delete;
+  BasicRegister(uint16_t read_port, uint16_t write_port):
+    _read_port(read_port), _write_port(write_port) {}
+
+  optional<uint8_t> read();
+  void write(uint8_t);
+
+
+private:
+    uint16_t _read_port;
+    uint16_t _write_port;
+};
+
+class ExtendedRegister : public VGARegister {
+public:
+  using Mode = enum {
+    Color,
+    Mono
+  };
+
+  ExtendedRegister() = delete;
+  ExtendedRegister(uint16_t read_mono_port, uint16_t read_color_port, 
+      uint16_t write_mono_port, uint16_t write_color_port):
+    _read_port_mono(read_mono_port), _read_port_color(read_color_port), 
+    _write_port_mono(write_mono_port), _write_port_color(write_color_port) {}
+
+  optional<uint8_t> read();
+  void write(uint8_t);
+
+  void setMode(Mode mode) {
+    _mode = mode;
+  }
+
+  Mode mode() {
+    return _mode;
+  }
+
+private:
+    uint16_t _read_port_mono;
+    uint16_t _read_port_color;
+    uint16_t _write_port_mono;
+    uint16_t _write_port_color;
+    Mode _mode;
+};
+
+class IndexedRegister : public VGARegister {
+public:
+  IndexedRegister() = delete;
+  IndexedRegister(uint16_t address_port, uint16_t data_port, uint8_t index):
+    _address_port(address_port), _data_port(data_port), _index(index) {}
+
+  optional<uint8_t> read();
+  void write(uint8_t);
+
+protected:
+    uint16_t _address_port;
+    uint16_t _data_port;
+    uint8_t  _index;
+};
+
+class CRTRegister : public VGARegister {
+  using Pair = struct {
+    uint16_t addressPort;
+    uint16_t dataPort;
+  };
+
+  CRTRegister() = delete;
+  CRTRegister(uint16_t pair0_address, uint16_t pair0_data, 
+      uint16_t pair1_address, uint16_t pair1_data, uint8_t index):
+    _pair0({.addressPort = pair0_address, .dataPort = pair0_data}), 
+    _pair1({.addressPort = pair1_address, .dataPort = pair1_data}),
+    _index(index) {}
+
+  optional<uint8_t> read();
+  void write(uint8_t);
+
+private:
+    Pair _pair0;
+    Pair _pair1;
+    uint8_t _index;
+};
+
+/*
+ * AttributeRegister has the same fields as IndexedRegister
+ * but AttributeRegister has a more confusing way of using them
+ * so they will be processed differently.
+ */
+class AttributeRegister : public IndexedRegister {
+public:
+  AttributeRegister() = delete;
+  AttributeRegister(uint16_t address_port, uint16_t data_port, uint8_t index):
+    IndexedRegister(address_port, data_port, index) {}
+
+  optional<uint8_t> read();
+  void write(uint8_t);
+};
+
+}; //Namespace kernel end
